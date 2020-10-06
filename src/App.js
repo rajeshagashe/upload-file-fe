fdbcd5fa5c785b579fc49607fd4c68e0873d312c
Rajesh Agashe <rajeshagashe94@gmail.com>
2020-10-06 20:16:51 +0530

    File upload working.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
 
 export class App extends React.Component {
   constructor(props){
@@ -8,10 +7,10 @@ export class App extends React.Component {
     this.state = {
       isButtonClicked : false
     }
-    this._onButtonClcik = this._onButtonClcik.bind(this)
+    this._onButtonClick = this._onButtonClick.bind(this)
   }
 
-  _onButtonClcik(){
+  _onButtonClick(){
     this.setState({
       isButtonClicked : true
     })
@@ -21,12 +20,14 @@ export class App extends React.Component {
     if (!this.state.isButtonClicked){
       return (
         <UploadButton 
-          onClick={this._onButtonClcik}
+          onClick={this._onButtonClick}
         />
       );
     }
     else{
-      return <button />
+      return (
+      <FileUpload/>
+      );
     }
   }
 }
@@ -53,3 +54,86 @@ class UploadButton extends React.Component{
 }
 
 export default App;
+
+class FileUpload extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      preSignedUrl : 
+      {
+        data:
+        {
+          url : "url"
+        }
+      },
+      fileUpload : false
+    }
+    this.submitFile = this.submitFile.bind(this)
+  }
+  
+  async componentDidMount(){
+    this.setState(
+      {
+        preSignedUrl : 
+          await axios.get(
+            `http://127.0.0.1:5000/file_upload/get_presigned_url`
+          )
+      }
+    )
+  }
+
+  async submitFile() {
+    try {
+      if (!this.state.file) {
+        throw new Error('Select a file first!');
+      }
+      const formData = new FormData();
+      Object.keys(this.state.preSignedUrl.data.fields).forEach(key => {
+        formData.append(key, this.state.preSignedUrl.data.fields[key]);
+      });
+      formData.append('file', this.state.file[0]);
+      
+      await axios.post(this.state.preSignedUrl.data.url, formData);
+      this.setState({fileUpload: true})
+      console.log("iplaegusCBHisLUFEGIYASLRJKFBWIUESLFJCBUILCBHUILJK")
+
+    } catch (error) {
+        console.log(error)
+    }
+  };
+
+  render(){
+    if(!this.state.fileUpload){
+      console.log(this.state.fileUpload)
+      return (
+        <form
+          style={{
+            bottom: 150,
+            left: 400,
+            position: "fixed",
+          }} 
+          onSubmit={this.submitFile}
+          // onClick={this.setState({fileUpload: true})}
+        >
+          <label>Upload file</label>
+          <input type="file" onChange={event => this.setState({file : event.target.files})} />
+          <button type="submit"  onClick={this.setState({fileUpload: true})}>Send</button>
+      </form>
+      );
+    }
+    else{
+      return (
+        <SuccessMessage />
+      )
+    }
+  }
+}
+// };
+
+class SuccessMessage extends React.Component {
+  render(){
+    return(
+      <h1>FILE UPLOADED SUCCESSFULLY</h1>
+      )
+  }
+}
