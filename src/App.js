36648094092719714f0e8ee863d4af977b8194ff
Rajesh Agashe <rajeshagashe94@gmail.com>
2020-10-06 14:50:03 +0530

    upload button component.

--- src/App.js
+++ src/App.js
@@ -2,25 +2,54 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      isButtonClicked : false
+    }
+    this._onButtonClcik = this._onButtonClcik.bind(this)
+  }
+
+  _onButtonClcik(){
+    this.setState({
+      isButtonClicked : true
+    })
+  }
+  
+  render(){
+    if (!this.state.isButtonClicked){
+      return (
+        <UploadButton 
+          onClick={this._onButtonClcik}
+        />
+      );
+    }
+    else{
+      return <button />
+    }
+  }
+}
+
+class UploadButton extends React.Component{
+  render(){
+    return (
+      <button 
+        style={{
+          height: 100,
+          width:500,
+          bottom: 150,
+          left: 400,
+          position: "fixed",
+          background: "white",
+          color: "blue"
+        }}
+        onClick={this.props.onClick}
+      >
+        <h1>UPLOAD FILE</h1>
+      </button>
+    );
+  }
 }
 
 export default App;
